bbebb256aa828ef98d04cf2f63b875fee82b8280
Stephen Appiah <appstephen8@gmail.com>
2020-11-24 06:33:49 +0000

    still settings to deploy

--- homestud/homestud/wsgi.py
+++ homestud/homestud/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.production')
 
 application = get_wsgi_application()
