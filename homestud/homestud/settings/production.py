6ddafb509a661603c3c191a5353327fee4092b55
Stephen Appiah <appstephen8@gmail.com>
2020-11-24 08:48:06 +0000

    secret key config

--- homestud/homestud/settings/production.py
+++ homestud/homestud/settings/production.py
@@ -4,10 +4,7 @@ env = environ.Env()
 # reading .env file
 environ.Env.read_env(env_file='.env')
 
-
-
-
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'my-secret-key')
 
 DEBUG = False
 
