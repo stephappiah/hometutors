fa5c9b98ac85787bf488535d795d73d8e665847f
Stephen Appiah <appstephen8@gmail.com>
2020-11-26 05:42:04 +0000

    heroku geo/gdal config

--- homestud/homestud/settings/production.py
+++ homestud/homestud/settings/production.py
@@ -1,5 +1,10 @@
 from .base import *
+
 import django_heroku
+from os import environ
+
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
 
 SECRET_KEY = config('SECRET_KEY')
