84f1c59aa610836f2a2620765ec74dfdfc11ac70
Stephen Appiah <appstephen8@gmail.com>
2021-01-05 08:13:38 +0000

    fuck onmessage

--- homestud/chat/static/js/alertSocket.js
+++ homestud/chat/static/js/alertSocket.js
@@ -51,9 +51,6 @@ alertSocket.onmessage = function(e) {
   // Highlight it
   $last_room.find('.chat-list-item').css('font-weight', 'bold');
 
-  //mycode
-  $('.mini-text').css('color', 'red');
-
   // Add the new message preview
   updateOpponentMessagePreview(received_room_id, sender, message);
 };
