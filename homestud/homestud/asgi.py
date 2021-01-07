d2bb0b96c01cd6d0f10c05c9e712829d5d8dd652
Stephen Appiah <appstephen8@gmail.com>
2021-01-07 08:22:54 +0000

    prod settings

--- homestud/homestud/asgi.py
+++ homestud/homestud/asgi.py
@@ -4,8 +4,8 @@ import django
 from channels.routing import get_default_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.development')
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.production')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.production')
 
 django.setup()
 application = get_default_application()
