e73abda11c92a36e885b0cba3081eb4c74fa7449
Stephen Appiah <appstephen8@gmail.com>
2021-01-28 19:44:10 +0000

    use dkim backend

--- homestud/homestud/settings/production.py
+++ homestud/homestud/settings/production.py
@@ -68,7 +68,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # for dkim backend
 EMAIL_BACKEND = 'django_dkim.backends.smtp.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtpout.secureserver.net'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
