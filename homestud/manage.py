19f32d91f07449f09275a248f210ee2430cd6e66
Stephen Appiah <appstephen8@gmail.com>
2021-02-09 20:57:49 +0000

    manage.py update

--- homestud/manage.py
+++ homestud/manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.development')
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.production')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homestud.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
