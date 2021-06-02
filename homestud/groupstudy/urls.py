2b223308601cdb882435eb17914de245576935e1
stephappiah <appstephen8@gmail.com>
2021-06-02 12:31:50 +0000

    added filtering functionality to group classes

--- homestud/groupstudy/urls.py
+++ homestud/groupstudy/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import GroupClassView, GroupClassUpdate, GroupClassDelete, GroupClassList, ShowGroupClass
+from .views import GroupClassView, GroupClassUpdate, GroupClassDelete, GroupClassList, ShowGroupClass, FilterSearch
 
 app_name = 'groupstudy'
 
 urlpatterns = [
 
     path('', ShowGroupClass.as_view(), name='classes'),
+    path('class/filter', FilterSearch, name='filter'),
     path('class/create', GroupClassView.as_view(), name="create-group"),
     path('class/<int:pk>/update', GroupClassUpdate.as_view(), name='group_class_update'),
     path('class/<int:pk>/delete/', GroupClassDelete.as_view(), name='group_class_delete'),
--- homestud/groupstudy/views.py
+++ homestud/groupstudy/views.py
@@ -5,9 +5,15 @@ from django.views.generic.edit import BaseDeleteView
 from .models import GroupClass
 from .forms import GroupClassForm
 from django.urls import reverse_lazy, reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 import json
 from findtutors.courses import courses_choices, programmes_choices
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import fromstr, Point
+from django.contrib.gis.measure import D
+from django.core.paginator import Paginator
+from django.template.loader import render_to_string, get_template
+
 
 class GroupClassView(LoginRequiredMixin, CreateView):
     model = GroupClass
@@ -94,48 +100,89 @@ class ShowGroupClass(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # check if search form has coordinates
-        # else use coordinates from session
-        def get_latitude():
-            if self.request.GET.get('latitude') is not None or '':
-                # coordinates from search form
-                latitude = self.request.GET.get('latitude')
-                return latitude
-            else:
-                # coordinates from session
-                latitude = self.request.session['lat']
-                return latitude
-
-        def get_longitude():
-            if request.GET.get('longitude') is not None or '':
-                # coordinates from search form
-                longitude = self.request.GET.get('longitude')
-                return longitude
-            else:
-                # coordinates from session
-                longitude = self.request.session['lon']
-                return longitude
-    
-
+        qs = qs = GroupClass.objects.all()
+        
+        context['total_groups'] = qs.count()
         context['programme_list'] = json.dumps(dict(programmes_choices))
         context['course_list'] = json.dumps(dict(courses_choices))
+
+        print(qs.count())
         return context
 
     def get_queryset(self):
         current_user = self.request.user
+        qs = GroupClass.objects.all()
+        paginator = Paginator(qs, 3) #show 10 tutors per page
 
-        
-        return GroupClass.objects.filter(tutor=current_user)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return page_obj
 
 
+def FilterSearch(request):
+    #for dropdown
+    programme_list = json.dumps(dict(programmes_choices))
+    course_list = json.dumps(dict(courses_choices))
 
-    def render_to_response(self, context, **response_kwargs):
-        """ Allow AJAX requests to be handled more gracefully """
-        if self.request.is_ajax():
-            context = self.get_context_data(**response_kwargs)
-            rendered = render_to_string(self.template_name, context, request=self.request)
+    # check if search form has coordinates
+    # else use coordinates from session
+    def get_latitude():
+        if request.GET.get('latitude') is not None or '':
+            # coordinates from search form
+            latitude = request.GET.get('latitude')
 
-            return JsonResponse({'update_form': rendered}, safe=False, **response_kwargs)
+            # Store coordinates in user session
+            request.session['lat'] = float(latitude)
+
+            return latitude
         else:
-            return super(ShowGroupClass, self).render_to_response(context, **response_kwargs)
+            # coordinates from session
+            latitude = request.session['lat']
+            return latitude
+
+    def get_longitude():
+        if request.GET.get('longitude') is not None or '':
+            # coordinates from search form
+            longitude = request.GET.get('longitude')
+
+            # Store coordinates in user session
+            request.session['lon'] = float(longitude) 
+
+            return longitude
+        else:
+            # coordinates from session
+            longitude = request.session['lon']
+            return longitude
+    
+    # get coordinates
+    longitude = float(get_longitude())
+    latitude = float(get_latitude())
+    print(longitude, latitude)
+    user_location = Point(longitude, latitude, srid=4326)
+    # Queryset filtered within a distance of 100km; annotated and orderd by distance
+    dist = Distance('location', user_location)
+    qs = GroupClass.objects.filter(location__distance_lte=(user_location, D(km=100))).annotate(distance=dist).order_by('distance')
+
+    # Grab search fields
+    programme = request.GET.get('programme')
+    course = request.GET.get('course')
+    
+    if programme != '' and programme is not None:
+        qs = qs.filter(tutoring_programs__icontains=programme)
+
+    if course != '' and course is not None:
+        qs = qs.filter(courses_subjects__icontains=course)
+
+    paginator = Paginator(qs, 3) #show 10 tutors per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+                'group_classes': page_obj,
+                'total_groups': qs.count(),
+                'programme_list': programme_list,
+                'course_list': course_list
+    }
+    return render(request, 'groupstudy/show_group.html', context)
